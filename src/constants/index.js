2c9b3755d5f1adc33f2bcda5b52b6adc726b16c6
Henrrike Quarshie <henrrikequarshie@gmail.com>
2024-11-09 00:13:19 +0000

    done with categories

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+import food from "../../../../assets/images/pin-food.jpg";
+
+const K = {
+    CATEGORIES: [
+        {
+            cover: food,
+            title: "Wallpaper",
+        },
+    ]
+};
+
+
+export default K;
